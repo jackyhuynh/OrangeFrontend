669f57c7656adfbb45b246b3244ac4e35ee993c0
trucdev <jackyhuynh87@gmail.com>
2024-10-05 11:12:45 -0500

    MUI Sign-in implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import ProTip from './components/ProTip.jsx';
-import Copyright from './components/Copyright.jsx';
+import ProTip from './components/ProTip.js';
+import Copyright from './components/Copyright.js';
+import SignIn from "./components/login/SignIn.tsx";
 
 export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI Vite.js example
+          <SignIn/>
         </Typography>
         <ProTip />
         <Copyright />

12706e9e4596b61d974b13e1182cd2d04e04fd1f
trucdev <jackyhuynh87@gmail.com>
2024-10-26 17:44:43 -0500

    WIP: update, the config and add clear form to sign up

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
-import {getAnalytics} from "firebase/analytics";
 import {getAuth} from 'firebase/auth'
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyChQF2bWXOB5V9SKcIqK07MHaB4ie18_5M",
     authDomain: "orange-448ca.firebaseapp.com",
@@ -17,9 +12,7 @@ const firebaseConfig = {
     measurementId: "G-9MLW8JJLB0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
-const analytics = getAnalytics(app);
 
 export {auth};
